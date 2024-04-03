4aaac55d4c2c093d10498a48cf63d5d3984b7c7a
chenzhongyou <chenzhongyou@topjoy.com>
2024-04-03 17:26:35 +0800

    feat: update UI

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 ## TODO
 
 - [ ] 宽度小于1400时把右边分类栏隐藏
+- [ ] 星空背景 https://codepen.io/shuangcs/pen/gKbQEj 
+- [ ] 添加nav导航栏
+- [ ] 修改blog列表样式
+- [ ] 添加blog阅读页面
+- [ ] 添加提交热点图
+- [ ] 添加随笔列表
+
+---
+
+Server
+
+-[ ] 添加管理后端
--- src/components/Home/Content.vue
+++ src/components/Home/Content.vue
@@ -3,63 +3,62 @@ import {echoed} from "../../stores/maind";
 import Summary from "./Summary.vue";
 import Tags from "./Tags.vue";
 import Classification from "./Classification.vue";
-
 const data = echoed()
 
 let blogs = data.test.blogs
 </script>
 
 <template>
-  <main class="main">
-
-
-      <!-- 导航栏 -->
-      <div class="nav">
-
+  <div style="display: flex; flex-direction: column">
+    <div class="nav">
+      <div style="height: 50px">
+        标签
       </div>
-
-      <!-- summary -->
-    <div class="main-panel">
-      <div class="summarys">
-        <Summary v-for="blog in blogs" :title="blog.title" :description="blog.description"/>
-      </div>
-
-      <div class="modules"> <!--tag、分类等模块-->
-        <Classification class="tags"/>
-        <Tags class="tags"/>
+      <div style="height: 50px">
+        分类
       </div>
     </div>
+    <main class="main-panel">
+      <div class="main-panel">
+        <div class="summarys">
+          <Summary v-for="blog in blogs" :title="blog.title" :description="blog.description"/>
+        </div>
 
+        <div class="modules"> <!--tag、分类等模块-->
+          <Classification class="tags"/>
+          <Tags class="tags"/>
+        </div>
+      </div>
+    </main>
+  </div>
 
-  </main>
 </template>
 
 <style scoped>
-.main{
-  display: flex;
-  flex-direction: column;
-}
-
 .nav{
-  display: block;
-  width: 100%;
-  height: 80px;
-  background: #646cff;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: end;
+  margin: 10px 10px 0 10px;
+  height: 60px;
+  border-radius: 20px;
+  //background: #ffffff77;
 }
 
 .main-panel {
   /* 其他样式 */
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
+  height: 100%;
 }
 
-.summarys {
+.summarys{
+  display: block;
   overflow-y: auto; /* 或 overflow: auto; 如果需要同时考虑水平和垂直滚动 */
   scrollbar-width: none; /* 针对Firefox等支持该属性的浏览器 */
   -ms-overflow-style: none; /* 针对IE/Edge */
 
   /* Webkit内核浏览器（如Chrome、Safari）隐藏滚动条 */
-
   &::-webkit-scrollbar {
     width: 0; /* 隐藏水平滚动条 */
     height: 0; /* 隐藏垂直滚动条 */
@@ -68,14 +67,13 @@ let blogs = data.test.blogs
   flex: 5;
   margin: 10px;
 }
-
-.tags {
+.tags{
   width: 80%;
   margin: 10px;
   margin-bottom: 20px;
 }
 
-.modules {
+.modules{
   display: flex;
   flex-direction: column;
   flex: 2;
