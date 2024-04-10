dd8f152374ddac371e009a2a5291c53e980b13a8
chenzhongyou <chenzhongyou@topjoy.com>
2024-04-10 18:57:52 +0800

    Update BlogView.vue

--- src/components/BlogView/BlogView.vue
+++ src/components/BlogView/BlogView.vue
@@ -24,8 +24,8 @@ onMounted(() => {
     // 编辑器中默认展示的文本
     value:content.value,
     // 设置编辑器的宽高
-    height: '100vh',
-    width: '50vw',
+    height: '100%',
+    width: '50%',
     // 设置工具栏中展示的工具
     toolbar: ['emoji', 'br', 'bold', '|', 'line','quote','list','check'],
     // 编辑器失去焦点后的回调函数
@@ -45,10 +45,24 @@ function save(md: string) {
 </script>
 
 <template>
-  <div id="vditor"></div>
+  <div class="editor_box">
+    <div style="width: 90%;height: 40px;line-height: 30px;padding-top: 5px;padding-bottom: 5px;margin: 0 auto;">
+      <a style="text-decoration: none;cursor: pointer">
+        <span>✨保存</span>
+      </a>
+    </div>
+    <div id="vditor"></div>
+  </div>
+
 </template>
 
 <style scoped>
-
+.editor_box{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
 </style>
