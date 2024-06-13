43f5e2a09b81f7b2586b5686e20f542e65d3cabd
chenzhongyou <chenzhongyou@topjoy.com>
2024-06-13 18:50:42 +0800

    add highlightjs

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import App from './App.vue'
 import router from "./route/routes.ts";
 import net from "./api/net.ts";
 
+
+// 引入css
+import './assets/css/animation.css'
+import './assets/css/index.css'
+import './assets/css/tocbot.css'
+import './assets/css/color.css'
+import './assets/css/markdown-highlight.css'
+import './assets/css/font-awesome.min.css'
+
 const app = createApp(App)
 app.use(createPinia()).use(router).mount('#app')
 app.config.globalProperties.$net=net;  //配置axios的全局引用
